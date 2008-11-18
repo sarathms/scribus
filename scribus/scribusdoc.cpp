sarathms/scribus

commit 4592aef22cb8c105da0da7478189cff635b107fa

Added "Inline Images" these are small images which are directly embedded in the XML when saving a Document. When reading such a Document they are extracted into temporary files which are deleted if the corresponding PageItem is deleted or another image is loaded into the frame. These inline images are currently only used by the SVG Importer.
ScribusXMLDoc::WriteElem() adds now a small preview image to the xml, this image is used by ScPreview to render
the icons for the scrapbook.

git-svn-id: file:///svn/Scribus/trunk/Scribus@12795 11d20701-8431-0410-a711-e3c959e3b870

scribus/scribusdoc.cpp
@@ -3834,7 +3834,13 @@ bool ScribusDoc::loadPict(QString fn, PageItem *pageItem, bool reload, bool show
 	if (!reload)
 	{
 		if ((ScCore->fileWatcher->files().contains(pageItem->Pfile) != 0) && (pageItem->PicAvail))
+		{
 			ScCore->fileWatcher->removeFile(pageItem->Pfile);
+			if (pageItem->tempImageFile != NULL)
+				delete pageItem->tempImageFile;
+			pageItem->tempImageFile = NULL;
+			pageItem->isInlineImage = false;
+		}
 	}
 	if(!pageItem->loadImage(fn, reload, -1, showMsg))
 	{
@@ -7169,6 +7175,8 @@ void ScribusDoc::itemSelection_DeleteItem(Selection* customSelection, bool force
 				target = Pages->at(0);
 			undoManager->action(target, is, currItem->getUPixmap());
 		}
+		else
+			delete currItem;
 	}
 	itemSelection->delaySignalsOff();
 	updateFrameItems();

scribus/collect4output.cpp
@@ -189,20 +189,23 @@ void CollectForOutput::processItem(PageItem *ite)
 	if (ite->asImageFrame())
 	{
 		/* hack for subsequent c4o "./" -> "/doc/full/path" */
-		QString ofName(ite->Pfile);
-		QFileInfo itf = QFileInfo(ofName);
-		if (!itf.exists())
+		if (!ite->isInlineImage)
 		{
-			ofName = QDir::convertSeparators(PrefsManager::instance()->documentDir() + "/" + ofName);
-			itf.setFile(ofName);
-		}
+			QString ofName(ite->Pfile);
+			QFileInfo itf = QFileInfo(ofName);
+			if (!itf.exists())
+			{
+				ofName = QDir::convertSeparators(PrefsManager::instance()->documentDir() + "/" + ofName);
+				itf.setFile(ofName);
+			}
 		// end of hack
-		if (itf.exists())
-		{
-			QString oldFile = ofName;
-			ite->Pfile = collectFile(oldFile, itf.fileName());
-			ScCore->fileWatcher->removeFile(oldFile);
-			ScCore->fileWatcher->addFile(ite->Pfile);
+			if (itf.exists())
+			{
+				QString oldFile = ofName;
+				ite->Pfile = collectFile(oldFile, itf.fileName());
+				ScCore->fileWatcher->removeFile(oldFile);
+				ScCore->fileWatcher->addFile(ite->Pfile);
+			}
 		}
 	}
 	if (ite->asTextFrame())
@@ -212,13 +215,16 @@ void CollectForOutput::processItem(PageItem *ite)
 			QFileInfo itf;
 			if (!ite->Pfile.isEmpty())
 			{
-				itf = QFileInfo(ite->Pfile);
-				if (itf.exists())
+				if (!ite->isInlineImage)
 				{
-					QString oldFile = ite->Pfile;
-					ite->Pfile = collectFile(oldFile, itf.fileName());
-					ScCore->fileWatcher->removeFile(oldFile);
-					ScCore->fileWatcher->addFile(ite->Pfile);
+					itf = QFileInfo(ite->Pfile);
+					if (itf.exists())
+					{
+						QString oldFile = ite->Pfile;
+						ite->Pfile = collectFile(oldFile, itf.fileName());
+						ScCore->fileWatcher->removeFile(oldFile);
+						ScCore->fileWatcher->addFile(ite->Pfile);
+					}
 				}
 			}
 			if (!ite->Pfile2.isEmpty())

scribus/pageitem.cpp
@@ -67,6 +67,7 @@ for which a new license (GPL+exception) is in place.
 #include "util_formats.h"
 #include "util_math.h"
 #include "util_text.h"
+#include "util_file.h"
 #ifdef HAVE_CAIRO
 	#include <cairo.h>
 #endif
@@ -245,6 +246,23 @@ PageItem::PageItem(const PageItem & other)
 	AnName += tmp.setNum(m_Doc->TotalItems);
 	uniqueNr = m_Doc->TotalItems;
 	invalid = true;
+	if (other.isInlineImage)
+	{
+		QFileInfo inlFi(Pfile);
+		QString ext = inlFi.suffix();
+		tempImageFile = new QTemporaryFile(QDir::tempPath() + "/scribus_temp_XXXXXX." + ext);
+		tempImageFile->open();
+		QString fileName = getLongPathName(tempImageFile->fileName());
+		tempImageFile->close();
+		copyFile(Pfile, fileName);
+		Pfile = fileName;
+		isInlineImage = true;
+	}
+	else
+	{
+		tempImageFile = NULL;
+		isInlineImage = false;
+	}
 }
 
 
@@ -502,6 +520,8 @@ PageItem::PageItem(ScribusDoc *pa, ItemType newType, double x, double y, double 
 			)
 			pageItemAttributes.append(*objAttrIt);
 	}
+	tempImageFile = NULL;
+	isInlineImage = false;
 }
 
 void PageItem::setXPos(const double newXPos, bool drawingOnly)

scribus/pageitem.h
@@ -34,6 +34,7 @@ for which a new license (GPL+exception) is in place.
 #include <QRect>
 #include <QRectF>
 #include <QVector>
+#include <QTemporaryFile>
 
 #include "scribusapi.h"
 #include "annotation.h"
@@ -193,7 +194,11 @@ protected:
 	
 public:
 	PageItem(ScribusDoc *pa, ItemType newType, double x, double y, double w, double h, double w2, QString fill, QString outline);
-	~PageItem() {};
+	~PageItem()
+	{
+		if (tempImageFile != NULL)
+			delete tempImageFile;
+	}
 
 	static const Xml_string saxxDefaultElem;
 	static void  desaxeRules(const Xml_string& prefixPattern, desaxe::Digester& ruleset, Xml_string elemtag = saxxDefaultElem);
@@ -1254,6 +1259,10 @@ public:
 	 /** Line width */
 	double m_lineWidth;
 	double Oldm_lineWidth;
+
+	/** Inline Image */
+	bool isInlineImage;
+	QTemporaryFile *tempImageFile;
 	
 signals:
 	//Frame signals

scribus/pageitem_desaxe.cpp
@@ -167,7 +167,24 @@ static Xml_attr PageItemXMLAttributes(const PageItem* item)
 		latexframe = dynamic_cast<const PageItem_LatexFrame*>(item);
 	
 	if ((item->itemType()==PageItem::ImageFrame || item->itemType()==PageItem::TextFrame) && (!item->externalFile().isEmpty()) && !latexframe)
-		result.insert("image-file", Path2Relative(item->externalFile(), QDir::homePath()));
+	{
+		if (item->isInlineImage)
+		{
+			result.insert("image-file", "");
+			result.insert("isInlineImage", toXMLString(item->isInlineImage));
+			QFileInfo inlFi(item->Pfile);
+			result.insert("inlineImageExt", inlFi.suffix());
+			QFile inFil(item->Pfile);
+			if (inFil.open(QIODevice::ReadOnly))
+			{
+				QByteArray ba = qCompress(inFil.readAll()).toBase64();
+				result.insert("ImageData", QString(ba));
+				inFil.close();
+			}
+		}
+		else
+			result.insert("image-file", Path2Relative(item->externalFile(), QDir::homePath()));
+	}
 	if (!item->fileIconPressed().isEmpty())
 		result.insert("icon-pressed-file", Path2Relative(item->fileIconPressed(), QDir::homePath()));
 	if (!item->fileIconRollover().isEmpty())

scribus/pageitem_imageframe.cpp
@@ -139,6 +139,10 @@ void PageItem_ImageFrame::clearContents()
 	setFillTransparency(0.0);
 	setLineTransparency(0.0);
 	imageClip.resize(0);
+	if (tempImageFile != NULL)
+		delete tempImageFile;
+	tempImageFile = NULL;
+	isInlineImage = false;
 	//				emit UpdtObj(Doc->currentPage->pageNr(), ItemNr);
 }
 

scribus/plugins/fileloader/scribus134format/scribus134format.cpp
@@ -2023,7 +2023,8 @@ PageItem* Scribus134Format::PasteItem(QDomElement *obj, ScribusDoc *doc, const Q
 		currItem = doc->Items->at(z);
 		currItem->setImageXYScale(scx, scy);
 		currItem->setImageXYOffset(obj->attribute("LOCALX").toDouble(), obj->attribute("LOCALY").toDouble());
-		if (currItem->asLatexFrame()) {
+		if (currItem->asLatexFrame())
+		{
 			PageItem_LatexFrame *latexitem = currItem->asLatexFrame();
 			IT = obj->firstChild();
 			while(!IT.isNull())
@@ -2035,22 +2036,49 @@ PageItem* Scribus134Format::PasteItem(QDomElement *obj, ScribusDoc *doc, const Q
 					latexitem->setDpi(it.attribute("DPI").toInt());
 					latexitem->setUsePreamble(static_cast<bool>(it.attribute("USE_PREAMBLE").toInt()));
 					QDomElement property = it.firstChildElement("PROPERTY");
-					while (!property.isNull()) {
+					while (!property.isNull())
+					{
 						QString name = property.attribute("name");
 						QString value = property.attribute("value");
 						property = property.nextSiblingElement("PROPERTY");
 						if (name.isEmpty()) continue;
 						latexitem->editorProperties[name] = value;
-					} 
-					
+					}
 					QString temp = it.text();
 					latexitem->setFormula(temp, false);
 					
 				}
 				IT=IT.nextSibling();
 			}
-		} else {
-			currItem->Pfile = Relative2Path(obj->attribute("PFILE"), baseDir);
+		}
+		else
+		{
+			bool inlineF = static_cast<bool>(obj->attribute("isInlineImage", "0").toInt());
+			QString dat = obj->attribute("ImageData", "");
+			QByteArray inlineImageData;
+			inlineImageData.append(dat);
+			QString inlineImageExt = obj->attribute("inlineImageExt", "");
+			if (inlineF)
+			{
+				if (inlineImageData.size() > 0)
+				{
+					currItem->tempImageFile = new QTemporaryFile(QDir::tempPath() + "/scribus_temp_XXXXXX." + inlineImageExt);
+					currItem->tempImageFile->open();
+					QString fileName = getLongPathName(currItem->tempImageFile->fileName());
+					currItem->tempImageFile->close();
+					inlineImageData = qUncompress(QByteArray::fromBase64(inlineImageData));
+					QFile outFil(fileName);
+					if (outFil.open(QIODevice::WriteOnly))
+					{
+						outFil.write(inlineImageData);
+						outFil.close();
+						currItem->isInlineImage = true;
+						currItem->Pfile = fileName;
+					}
+				}
+			}
+			else
+				currItem->Pfile = Relative2Path(obj->attribute("PFILE"), baseDir);
 		}
 		currItem->IProfile  = obj->attribute("PRFILE","");
 		currItem->EmProfile = obj->attribute("EPROF","");

scribus/plugins/fileloader/scribus134format/scribus134format_save.cpp
@@ -1419,7 +1419,24 @@ void Scribus134Format::SetItemProps(ScXmlStreamWriter& docu, PageItem* item, con
 	docu.writeAttribute("REXTRA",item->textToFrameDistRight());
 	docu.writeAttribute("FLOP",item->firstLineOffset()); // here I think this FLOP "cher à mon cœur" is legitimate!
 	if (((item->asImageFrame() && !item->asLatexFrame()) || (item->asTextFrame())) && (!item->Pfile.isEmpty()))
-		docu.writeAttribute("PFILE",Path2Relative(item->Pfile, baseDir));
+	{
+		if (item->isInlineImage)
+		{
+			docu.writeAttribute("PFILE", "");
+			docu.writeAttribute("isInlineImage", static_cast<int>(item->isInlineImage));
+			QFileInfo inlFi(item->Pfile);
+			docu.writeAttribute("inlineImageExt", inlFi.suffix());
+			QFile inFil(item->Pfile);
+			if (inFil.open(QIODevice::ReadOnly))
+			{
+				QByteArray ba = qCompress(inFil.readAll()).toBase64();
+				docu.writeAttribute("ImageData", QString(ba));
+				inFil.close();
+			}
+		}
+		else
+			docu.writeAttribute("PFILE",Path2Relative(item->Pfile, baseDir));
+	}
 	else
 		docu.writeAttribute("PFILE","");
 	if (!item->Pfile2.isEmpty())

commit 286e6bb023c1d3f86ded3f4a2255287830a817a9

fix #7619 : redo an object creation crashes the program, the crash was introduced by r12795
git-svn-id: file:///svn/Scribus/trunk/Scribus@12879 11d20701-8431-0410-a711-e3c959e3b870

scribus/scribusdoc.cpp
@@ -7175,7 +7175,7 @@ void ScribusDoc::itemSelection_DeleteItem(Selection* customSelection, bool force
 				target = Pages->at(0);
 			undoManager->action(target, is, currItem->getUPixmap());
 		}
-		else
+		else if (forceDeletion)
 			delete currItem;
 	}
 	itemSelection->delaySignalsOff();