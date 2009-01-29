sarathms/scribus

commit 19e8d497f77d48ce1f19a3ffe8bdb1be9758dd6a

Fixed Bug #7765 : "middle mouse panning (GIMP+InNkscape like)" and moved some code to the canvasmode base class.
git-svn-id: file:///svn/Scribus/trunk/Scribus@13124 11d20701-8431-0410-a711-e3c959e3b870

scribus/canvasmode_framelinks.cpp
@@ -66,15 +66,7 @@ inline bool CanvasMode_FrameLinks::GetItem(PageItem** pi)
 
 void CanvasMode_FrameLinks::drawControls(QPainter* p)
 {
-//	qDebug() << "CanvasMode_FrameLinks::drawControls";
-	if (m_canvas->m_viewMode.operItemMoving)
-	{
-		drawOutline(p);
-	}
-	else
-	{
-		drawSelection(p);
-	}
+	commonDrawControls(p);
 }
 
 void CanvasMode_FrameLinks::enterEvent(QEvent *)
@@ -129,30 +121,11 @@ void CanvasMode_FrameLinks::mouseDoubleClickEvent(QMouseEvent *m)
 
 void CanvasMode_FrameLinks::mouseMoveEvent(QMouseEvent *m)
 {
-// 	const double mouseX = m->globalX();
-// 	const double mouseY = m->globalY();
 	const FPoint mousePointDoc = m_canvas->globalToCanvas(m->globalPos());
 	
 	m->accept();
+	commonMouseMove(m);
 
-	if (m_canvas->m_viewMode.m_MouseButtonPressed && (m->buttons() & Qt::RightButton) && (m->modifiers() & Qt::ControlModifier))
-	{
-		if (m_doc->appMode != modePanning)
-		{
-			m_oldAppMode = m_doc->appMode;
-			m_ScMW->setAppMode(modePanning);
-		}
-	}
-	if (m_canvas->m_viewMode.m_MouseButtonPressed && (m_doc->appMode == modePanning))
-	{
-		double sc = m_canvas->scale();
-		int scroX = qRound((mousePointDoc.x() - Mxp) * sc);
-		int scroY = qRound((mousePointDoc.y() - Myp) * sc);
-		m_view->scrollBy(-scroX, -scroY);
-		Mxp = mousePointDoc.x();
-		Myp = mousePointDoc.y();
-		return;
-	}
 	if ((m_canvas->m_viewMode.m_MouseButtonPressed) && (m->buttons() & Qt::LeftButton))
 	{
 		SeRx = qRound(mousePointDoc.x()); //m_view->translateToDoc(m->x(), m->y()).x());
@@ -198,7 +171,8 @@ void CanvasMode_FrameLinks::mousePressEvent(QMouseEvent *m)
 	if (m->button() == Qt::MidButton)
 	{
 		m_view->MidButt = true;
-		m_view->DrawNew();
+		if (m->modifiers() & Qt::ControlModifier)
+			m_view->DrawNew();
 		return;
 	}
 	switch (m_doc->appMode)

scribus/canvasmode_objimport.cpp
@@ -115,30 +115,8 @@ void CanvasMode_ObjImport::mouseDoubleClickEvent(QMouseEvent *m)
 
 void CanvasMode_ObjImport::mouseMoveEvent(QMouseEvent *m)
 {
-// 	const double mouseX = m->globalX();
-// 	const double mouseY = m->globalY();
-	const FPoint mousePointDoc = m_canvas->globalToCanvas(m->globalPos());
-	
 	m->accept();
-
-	if (m_canvas->m_viewMode.m_MouseButtonPressed && (m->buttons() & Qt::RightButton) && (m->modifiers() & Qt::ControlModifier))
-	{
-		if (m_doc->appMode != modePanning)
-		{
-			m_oldAppMode = m_doc->appMode;
-			m_ScMW->setAppMode(modePanning);
-		}
-	}
-	if (m_canvas->m_viewMode.m_MouseButtonPressed && (m_doc->appMode == modePanning))
-	{
-		double sc = m_canvas->scale();
-		int scroX = qRound((mousePointDoc.x() - Mxp) * sc);
-		int scroY = qRound((mousePointDoc.y() - Myp) * sc);
-		m_view->scrollBy(-scroX, -scroY);
-		Mxp = mousePointDoc.x();
-		Myp = mousePointDoc.y();
-		return;
-	}
+	commonMouseMove(m);
 }
 
 void CanvasMode_ObjImport::mousePressEvent(QMouseEvent *m)
@@ -166,7 +144,8 @@ void CanvasMode_ObjImport::mousePressEvent(QMouseEvent *m)
 	if (m->button() == Qt::MidButton)
 	{
 		m_view->MidButt = true;
-		m_view->DrawNew();
+		if (m->modifiers() & Qt::ControlModifier)
+			m_view->DrawNew();
 		return;
 	}
 	if ((m->button() == Qt::LeftButton) && m_mimeData)

scribus/canvas.h
@@ -86,6 +86,7 @@ public:
 	Canvas(ScribusDoc* doc, ScribusView* parent);
 	
 	friend class ScribusView; // for now...
+	friend class CanvasMode;
 	friend class LegacyMode;
 	friend class CanvasMode_CopyProperties;
 	friend class CanvasMode_Edit;

scribus/canvasmode.cpp
@@ -37,6 +37,7 @@
 #include "selection.h"
 #include "scpainter.h"
 #include "scresizecursor.h"
+#include "scribus.h"
 #include "scribusview.h"
 #include "util_icon.h"
 
@@ -617,4 +618,32 @@ void CanvasMode::setResizeCursor(int how, double rot)
 			qApp->changeOverrideCursor(QCursor(Qt::SizeAllCursor));
 			break;
 	}
+}
+
+void CanvasMode::commonMouseMove(QMouseEvent *m)
+{
+	const FPoint mousePointDoc = m_canvas->globalToCanvas(m->globalPos());
+	if ((m_canvas->m_viewMode.m_MouseButtonPressed && (m->buttons() & Qt::RightButton) && (m->modifiers() & Qt::ControlModifier)) || ((!(m->modifiers() & Qt::ControlModifier)) && (m->buttons() & Qt::MidButton)))
+	{
+		if (m_doc->appMode != modePanning)
+			m_view->m_ScMW->setAppMode(modePanning);
+	}
+	if ((m_canvas->m_viewMode.m_MouseButtonPressed || m_view->MidButt) && (m_doc->appMode == modePanning))
+	{
+		double sc = m_canvas->scale();
+		int scroX = qRound((mousePointDoc.x() - Mxp) * sc);
+		int scroY = qRound((mousePointDoc.y() - Myp) * sc);
+		m_view->scrollBy(-scroX, -scroY);
+		Mxp = mousePointDoc.x();
+		Myp = mousePointDoc.y();
+		return;
+	}
+}
+
+void CanvasMode::commonDrawControls(QPainter* p)
+{
+	if (m_canvas->m_viewMode.operItemMoving)
+		drawOutline(p);
+	else
+		drawSelection(p);
 }

scribus/canvasmode.h
@@ -39,6 +39,7 @@ class  Canvas;
 struct CanvasViewMode;
 class  ScribusDoc;
 class  ScribusView;
+class ScribusMainWindow;
 class  PageItem;
 class PageItemPreview;
 
@@ -133,8 +134,11 @@ protected:
 	ScribusView * const m_view;	
 	Canvas * const m_canvas;
 	ScribusDoc * const m_doc;
+	double Mxp, Myp, Dxp, Dyp;
 	
 	void setResizeCursor(int how, double rot = 0.0);
+	void commonMouseMove(QMouseEvent *m);
+	void commonDrawControls(QPainter* p);
 	
 	private:
 		QMap<QString,QPen> m_pen;

scribus/canvasmode_copyproperties.cpp
@@ -57,15 +57,7 @@ inline bool CanvasMode_CopyProperties::GetItem(PageItem** pi)
 
 void CanvasMode_CopyProperties::drawControls(QPainter* p)
 {
-//	qDebug() << "CanvasMode_CopyProperties::drawControls";
-	if (m_canvas->m_viewMode.operItemMoving)
-	{
-		drawOutline(p);
-	}
-	else
-	{
-		drawSelection(p);
-	}
+	commonDrawControls(p);
 }
 
 void CanvasMode_CopyProperties::enterEvent(QEvent *)
@@ -121,31 +113,10 @@ void CanvasMode_CopyProperties::mouseDoubleClickEvent(QMouseEvent *m)
 
 void CanvasMode_CopyProperties::mouseMoveEvent(QMouseEvent *m)
 {
-// 	const double mouseX = m->globalX();
-// 	const double mouseY = m->globalY();
-	const FPoint mousePointDoc = m_canvas->globalToCanvas(m->globalPos());
-	
 	m->accept();
-
-	if (m_canvas->m_viewMode.m_MouseButtonPressed && (m->buttons() & Qt::RightButton) && (m->modifiers() & Qt::ControlModifier))
-	{
-		if (m_doc->appMode != modePanning)
-			m_ScMW->setAppMode(modePanning);
-	}
-	if (m_canvas->m_viewMode.m_MouseButtonPressed && (m_doc->appMode == modePanning))
-	{
-		double sc = m_canvas->scale();
-		int scroX = qRound((mousePointDoc.x() - Mxp) * sc);
-		int scroY = qRound((mousePointDoc.y() - Myp) * sc);
-		m_view->scrollBy(-scroX, -scroY);
-		Mxp = mousePointDoc.x();
-		Myp = mousePointDoc.y();
-		return;
-	}
+	commonMouseMove(m);
 	if ((m_canvas->m_viewMode.m_MouseButtonPressed) && (m->buttons() & Qt::LeftButton))
 	{
-//		double newX = qRound(mousePointDoc.x()); //m_view->translateToDoc(m->x(), m->y()).x());
-//		double newY = qRound(mousePointDoc.y()); //m_view->translateToDoc(m->x(), m->y()).y());
 		QPoint startP = m_canvas->canvasToGlobal(QPointF(Mxp, Myp));
 		m_view->redrawMarker->setGeometry(QRect(startP, m->globalPos()).normalized());
 		if (!m_view->redrawMarker->isVisible())
@@ -180,7 +151,8 @@ void CanvasMode_CopyProperties::mousePressEvent(QMouseEvent *m)
 	if (m->button() == Qt::MidButton)
 	{
 		m_view->MidButt = true;
-		m_view->DrawNew();
+		if (m->modifiers() & Qt::ControlModifier)
+			m_view->DrawNew();
 		return;
 	}
 	if (m->button() != Qt::LeftButton)

scribus/canvasmode_copyproperties.h
@@ -53,7 +53,7 @@ private:
 	void createContextMenu(PageItem *currItem, double mx, double my);
 
 	int frameResizeHandle;
-	double Mxp, Myp, Dxp, Dyp;
+//	double Mxp, Myp, Dxp, Dyp;
 	ScribusMainWindow* m_ScMW;
 };
 

scribus/canvasmode_create.cpp
@@ -227,12 +227,8 @@ void CreateMode::mouseMoveEvent(QMouseEvent *m)
 //	qDebug() << "legacy mode move:" << m->x() << m->y() << m_canvas->globalToCanvas(m->globalPos()).x() << m_canvas->globalToCanvas(m->globalPos()).y();
 //	emit MousePos(m->x()/m_canvas->scale(),// + m_doc->minCanvasCoordinate.x(), 
 //				  m->y()/m_canvas->scale()); // + m_doc->minCanvasCoordinate.y());
-	
-	if (m_MouseButtonPressed && (m->buttons() & Qt::RightButton) && (m->modifiers() & Qt::ControlModifier))
-	{
-		m_view->requestMode(modePanning);
-	}
-		
+
+	commonMouseMove(m);
 	if (GetItem(&currItem))
 	{
 		newX = qRound(mousePointDoc.x()); //m_view->translateToDoc(m->x(), m->y()).x());
@@ -336,7 +332,8 @@ void CreateMode::mousePressEvent(QMouseEvent *m)
 	if (m->button() == Qt::MidButton)
 	{
 		m_view->MidButt = true;
-		m_view->DrawNew();
+		if (m->modifiers() & Qt::ControlModifier)
+			m_view->DrawNew();
 		return;
 	}
 	

commit 173a4bbf29911845a392f5331d1e10e128e56a7a

fix breakage introduced by r13124
git-svn-id: file:///svn/Scribus/trunk/Scribus@13126 11d20701-8431-0410-a711-e3c959e3b870

scribus/canvasmode_framelinks.cpp
@@ -124,8 +124,25 @@ void CanvasMode_FrameLinks::mouseMoveEvent(QMouseEvent *m)
 	const FPoint mousePointDoc = m_canvas->globalToCanvas(m->globalPos());
 	
 	m->accept();
-	commonMouseMove(m);
 
+	if ((m_canvas->m_viewMode.m_MouseButtonPressed && (m->buttons() & Qt::RightButton) && (m->modifiers() & Qt::ControlModifier)) || ((!(m->modifiers() & Qt::ControlModifier)) && (m->buttons() & Qt::MidButton)))
+	{
+		if (m_doc->appMode != modePanning)
+		{
+			m_oldAppMode = m_doc->appMode;
+			m_ScMW->setAppMode(modePanning);
+		}
+	}
+	if (m_canvas->m_viewMode.m_MouseButtonPressed && (m_doc->appMode == modePanning))
+	{
+		double sc = m_canvas->scale();
+		int scroX = qRound((mousePointDoc.x() - Mxp) * sc);
+		int scroY = qRound((mousePointDoc.y() - Myp) * sc);
+		m_view->scrollBy(-scroX, -scroY);
+		Mxp = mousePointDoc.x();
+		Myp = mousePointDoc.y();
+		return;
+	}
 	if ((m_canvas->m_viewMode.m_MouseButtonPressed) && (m->buttons() & Qt::LeftButton))
 	{
 		SeRx = qRound(mousePointDoc.x()); //m_view->translateToDoc(m->x(), m->y()).x());

scribus/canvasmode_objimport.cpp
@@ -115,8 +115,28 @@ void CanvasMode_ObjImport::mouseDoubleClickEvent(QMouseEvent *m)
 
 void CanvasMode_ObjImport::mouseMoveEvent(QMouseEvent *m)
 {
+	const FPoint mousePointDoc = m_canvas->globalToCanvas(m->globalPos());
+	
 	m->accept();
-	commonMouseMove(m);
+
+	if (m_canvas->m_viewMode.m_MouseButtonPressed && (m->buttons() & Qt::RightButton) && (m->modifiers() & Qt::ControlModifier))
+	{
+		if (m_doc->appMode != modePanning)
+		{
+			m_oldAppMode = m_doc->appMode;
+			m_ScMW->setAppMode(modePanning);
+		}
+	}
+	if (m_canvas->m_viewMode.m_MouseButtonPressed && (m_doc->appMode == modePanning))
+	{
+		double sc = m_canvas->scale();
+		int scroX = qRound((mousePointDoc.x() - Mxp) * sc);
+		int scroY = qRound((mousePointDoc.y() - Myp) * sc);
+		m_view->scrollBy(-scroX, -scroY);
+		Mxp = mousePointDoc.x();
+		Myp = mousePointDoc.y();
+		return;
+	}
 }
 
 void CanvasMode_ObjImport::mousePressEvent(QMouseEvent *m)